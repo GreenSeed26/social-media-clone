7e29fa8ae2efab34a7df7bea99c67b239594bcc9
GreenSeed26 <christian.narte29@gmail.com>
2024-07-20 10:06:51 +0800

    .

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,13 @@
+import { AlertTriangle } from "lucide-react";
 import React from "react";
 
 function NotFound() {
-  return <div>Page will not be developed :&#40;</div>;
+  return (
+    <div>
+      {" "}
+      <AlertTriangle size={20} /> Page will not be developed :&#40;
+    </div>
+  );
 }
 
 export default NotFound;
